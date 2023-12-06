c084396cc32a432c6202ea38843367725a323544
Will Turner <rwturner17@gmail.com>
2023-12-06 15:49:22 -0500

    solve 1 b

--- src/day1.test.ts
+++ src/day1.test.ts
@@ -1,4 +1,4 @@
-import { solve, calibrate } from './day1';
+import { solve_a, transform, calibrate } from './day1';
 import { test, expect } from '@jest/globals';
 
 test('solves correctly on test text', () => {
@@ -7,8 +7,8 @@ test('solves correctly on test text', () => {
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet`.trim().split('\n');
-    console.log(test_text);
-    expect(solve(test_text)).toBe(142);
+    // console.log(test_text);
+    expect(solve_a(test_text)).toBe(142);
 })
 
 test('calibrates correctly on test lines', () => {
@@ -17,4 +17,9 @@ test('calibrates correctly on test lines', () => {
     expect(calibrate('pqr3stu8vwx')).toBe(38);
     expect(calibrate('a1b2c3d4e5f')).toBe(15);
     expect(calibrate('treb7uchet')).toBe(77);
+})
+
+test('transforms correctly on test lines', () => {
+    expect(transform('one')).toBe('o1e');
+    expect(transform('onefourm5qpfvdnbs')).toBe('o1ef4rm5qpfvdnbs');
 })
--- src/day1.ts
+++ src/day1.ts
@@ -8,7 +8,7 @@ export function calibrate(line: string): number {
     return number;
 }
 
-export function solve(lines: string[]): number {
+export function solve_a(lines: string[]): number {
     // for each line, get the calibration value
     const calibrations = lines.map(line => calibrate(line));
     console.log(calibrations);
@@ -18,13 +18,54 @@ export function solve(lines: string[]): number {
     return sum;
 }
 
-async function main(): Promise<number> {
+const NUMBER_WORDS_MAP = {
+    'one': 'o1e',
+    'two': 't2o',
+    'three': 't3e',
+    'four': 'f4r',
+    'five': 'f5e',
+    'six': 's6x',
+    'seven': 's7n',
+    'eight': 'e8t',
+    'nine': 'n9e',
+}
+
+export function transform(line: string): string {
+    // Transform number words like 'one' in to numbers like 'o1e'
+    let line_copy = line;
+    for (const [number_word, number_word_transform] of Object.entries(NUMBER_WORDS_MAP)) {
+        line_copy = line_copy.replaceAll(number_word, number_word_transform);
+    }
+    return line_copy;
+}
+
+export function solve_b(lines: string[]): number {
+    // for each line, transform number words into numbers
+    const transformed = lines.map(line => transform(line));
+    console.log('transformed:', transformed);
+    // then calibrate
+    const calibrations = transformed.map(line => calibrate(line));
+    console.log('calibrations: ', calibrations);
+    // then sum them all up
+    const sum = calibrations.reduce((a, b) => a + b, 0);
+    console.log('b_sum: ', sum);
+    return sum;
+}
+
+async function load(): Promise<string[]> {
     const data = fs.readFileSync('data/input01.txt');
     const text = data.toString();
     const lines = text.trim().split('\n');
-    console.log(lines);
-    const result = solve(lines);
-    return result
+    return lines;
+}
+
+async function main(): Promise<number[]> {
+    const lines = await load();
+    const result_a = solve_a(lines);
+    console.log(result_a);
+    const result_b = solve_b(lines)
+    console.log(result_b);
+    return [result_a, result_b]
 }
 
 if (require.main == module) {
